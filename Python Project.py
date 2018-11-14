e11daf35e0a05b1bf9b7e9ef61e8032146016322
Jaleel <jtf73@scarletmail.rutger.edu>
2018-11-14 11:40:39 -0800

    Got Response 200
    I was fiddling around with it and slowly losing my sanity in the process, but I somehow managed to get it to a Response 200.
    I know you wanted me to try something else, and I tried a lot of differennt things but I got it "working" with a different api.

--- Python Project.py
+++ Python Project.py
@@ -1,15 +1,20 @@
 import requests
-response = requests.get("https://api.iextrading.com/1.0.json")
-aresponse = requests.get()
-def mfdoom(l):
-    l = raw_input("What do you want to talk about?")
+url = ("https://api.iextrading.com/1.0/stock/aapl/chart.json")
+import json
+    #import simplejson as json
+    #parsed_json = json.loads(json_string)
+r = requests.get("https://api.iextrading.com/1.0/stock/aapl/chart")
+print(r)
+#aresponse = requests.get()
+#def mfdoom(l):
+#    l = raw_input("What do you want to talk about?")
     #This takes the users input, mainly the name of coompany's and their stock
     #As long as said company exist, It will print a respnse that continues on the code
-    if l 0: #Will change 0 later
-        print ("Well, I'll tell you what," + l +"ain't dead so they passed step one")
+#    if l 0: #Will change 0 later
+#        print ("Well, I'll tell you what," + l +"ain't dead so they passed step one")
 
-    print ("The most I can tell you about them is that they're doing fine-wait.")
-    if status of l > #is a positive increase
-        print ("Yeah, no, I was right, they're doing pretty well for themselves.")
-    if status of l < #is a decrease
-        print ("They're not doing too good nowadays, a bunch of bad choices, a changing market, just not adapting quick enough.")
+#    print ("The most I can tell you about them is that they're doing fine-wait.")
+#    if l("changeOverTime") >= 0 #is a positive increase
+    #    print ("Yeah, no, I was right, they're doing pretty well for themselves.")
+#    if l("changeOverTime":) < 0 #is a decrease
+    #    print ("They're not doing too good nowadays, a bunch of bad choices, a changing market, just not adapting quick enough.")
